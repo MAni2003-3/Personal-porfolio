73d253b9c50bdb7589a903cebb0862402adfda0f
mani <mani@gmail.com>
2024-11-15 00:03:29 +0530

    Updated HTML and CSS files, added new pages, and removed outdated file

--- Mani/mani.js
+++ Mani/mani.js
@@ -1,5 +1,5 @@
 var typed=new Typed(".text",{
-    strings:["Core Java","JDBC","Servelts","SQL","HTML","CSS","JavaScript"],
+    strings:["Core Java","JDBC","Servelts","Hibernate","SpringBoot","SQL","HTML","CSS","JavaScript"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
